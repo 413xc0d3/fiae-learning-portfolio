a14283d6d6df5e9cca8e70442b7aa6f1311c5886
413xc0d3 <a.bokk@web.de>
2026-08-19 10:36:56 +0200

    fix: sync Barista live demo

--- docs/barista/barista.js
+++ docs/barista/barista.js
@@ -3,6 +3,14 @@ console.log("Externe JS geladen ")
         let anzeige = document.getElementById("display");
         let btnHeizen = document.getElementById("btnHeizen");
         let btnStart = document.getElementById("btnStart");
+        let wasser = 1000;
+        let kaffee = 250;
+        let milch = 500;
+        let espressoWasser = 50;
+        let espressoKaffee = 25;
+        let latteWasser = 50;
+        let latteKaffee = 25;
+        let latteMilch = 150;
 
         let temperatur = 20;
         let wahl = "";
@@ -44,13 +52,44 @@ console.log("Externe JS geladen ")
             console.log("Start wurde gedrückt");
             // 1. Prüfung - Temperatur
             // 2. Prfüung - welches Getränk?
-            if(temperatur < 90){
+            if(temperatur < 90)
+            {
                 anzeige.innerHTML = "Fehler: Maschine muss noch heizen";
-            }else if(wahl == ""){
+            }else if(wahl == "")
+            {
                 anzeige.innerHTML = "Fehler: Keine Auswahl getroffen ";
             }
-            else{
-                anzeige.innerHTML = (":-)");
+            else if (wahl == "Espresso" && wasser < espressoWasser)
+            {
+                anzeige.innerHTML = "Fehler: Nicht genug Wasser für Espresso";
+            }
+            else if (wahl == "Espresso" && kaffee < espressoKaffee)
+            {
+                anzeige.innerHTML = "Fehler: Nicht genug Kaffee für Espresso";
+            }
+            else if (wahl == "Latte" && wasser < latteWasser)
+            {
+                anzeige.innerHTML = "Fehler: Nicht genug Wasser für Latte";
+            }
+            else if (wahl == "Latte" && kaffee < latteKaffee)
+            {
+                anzeige.innerHTML = "Fehler: Nicht genug Kaffee für Latte";
+            }
+            else if (wahl == "Latte" && milch < latteMilch)
+            {
+                anzeige.innerHTML = "Fehler: Nicht genug Milch für Latte";
+            }
+            else
+            {
+                if (wahl == "Espresso") {
+                    wasser -= espressoWasser;
+                    kaffee -= espressoKaffee;
+                } else if (wahl == "Latte") {
+                    wasser -= latteWasser;
+                    kaffee -= latteKaffee;
+                    milch -= latteMilch;
+                }
+                anzeige.innerHTML = ("Fertig! Wasser: " +wasser+ " ml, Kaffee: "+kaffee+" g, Milch: "+milch+" ml");
             }
         }
 
